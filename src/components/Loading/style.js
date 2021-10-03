f09dc518d319ab39b6c4cb31ebd120df148618b7
HectorMtz22 <hec.gre81@gmail.com>
2021-10-03 16:07:20 -0500

    Styling

--- src/components/Loading/style.js
+++ src/components/Loading/style.js
@@ -2,25 +2,31 @@ import styled from "styled-components";
 
 export const Div = styled.div`
   background-color: #5555ff99;
-  width: 80%;
+  width: 60%;
   max-width: 1000px;
-  height: 80vh;
   position: absolute;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
   color: white;
-  padding: 2rem;
+  padding: 1rem;
   z-index: 1000;
 `;
 
 export const Iframe = styled.iframe`
-  width: 100%;
+  width: 70%;
+  display: block;
+  margin: 0px auto;
   aspect-ratio: 16/9;
+
+  @media (min-width: 1500px) {
+    width: 100%;
+  }
 `;
 
 export const Button = styled.button`
-  background-color: #55a5ff;
+  width: 100%;
+  background-color: #9c4538;
   color: white;
   border: none;
   border-radius: 5px;
@@ -29,6 +35,6 @@ export const Button = styled.button`
   cursor: pointer;
   transition: 200ms ease;
   &:hover {
-    background-color: #55a599;
+    background-color: #5c2921;
   }
 `;
