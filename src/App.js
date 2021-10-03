39c9d7f31f8122b4b02693d58283f00bfe0078d9
HectorMtz22 <hec.gre81@gmail.com>
2021-10-02 19:25:53 -0500

    Fix Latitude and Longitude
    Co-authored-by: Sebastian Marines <sebastianmarines@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { Cartesian3 } from "cesium";
+import { Cartesian3, Color } from "cesium";
 import { Viewer, Entity } from "resium";
 import * as satellite from "satellite.js";
 
@@ -26,9 +26,9 @@ let tle_l2 =
   "2 00900  90.1693  36.2849 0028983  80.6350 341.5474 13.73592980835239";
 
 export default function App() {
-  const [longitude, setLongitude] = useState();
-  const [latitude, setLatitude] = useState();
-  const [height, setHeight] = useState();
+  const [longitude, setLongitude] = useState(0);
+  const [latitude, setLatitude] = useState(0);
+  const [height, setHeight] = useState(0);
 
   useEffect(() => {
     const interval = setInterval(() => {
@@ -36,17 +36,24 @@ export default function App() {
       var positionAndVelocity = satellite.propagate(satrec, new Date());
       var positionEci = positionAndVelocity.position;
       // var velocityEci = positionAndVelocity.velocity;
-
+      var observerGd = {
+        longitude: satellite.degreesToRadians(-122.0308),
+        latitude: satellite.degreesToRadians(36.9613422),
+        height: 0.37,
+      };
       var gmst = satellite.gstime(new Date());
+
+      var positionEcf = satellite.eciToEcf(positionEci, gmst);
+      var lookAngles = satellite.ecfToLookAngles(observerGd, positionEcf);
       var positionGd = satellite.eciToGeodetic(positionEci, gmst);
       var longitudeRad = positionGd.longitude,
         latitudeRad = positionGd.latitude,
         heightRad = positionGd.height;
 
-      setLatitude(satellite.degreesLong(longitudeRad));
-      setLongitude(satellite.degreesLat(latitudeRad));
-      setHeight(heightRad);
-    }, 1000);
+      setLongitude(satellite.degreesLong(longitudeRad));
+      setLatitude(satellite.degreesLat(latitudeRad));
+      setHeight(positionGd.height * 1000);
+    }, 100);
     return () => clearInterval(interval);
   }, []);
 
@@ -56,7 +63,7 @@ export default function App() {
         <Entity
           name="satelite jr"
           position={Cartesian3.fromDegrees(longitude, latitude, height)}
-          point={{ pixelSize: 10 }}
+          point={{ pixelSize: 10, color: Color.Red }}
         />
       ) : (
         <div></div>
