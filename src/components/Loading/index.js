95c82c7d6c95aa88b5530e821c9935fb7ed1e7f7
HectorMtz22 <hec.gre81@gmail.com>
2021-10-03 15:53:34 -0500

    Loading Experience

--- src/components/Loading/index.js
+++ src/components/Loading/index.js
@@ -1,13 +1,26 @@
-import { Div } from "./style";
+import { Button, Div, Iframe } from "./style";
 const Loading = ({ clickHandler, isLoading }) => {
   return (
     <Div>
       <h1>Mapping Space Trash in Real Time</h1>
-      {isLoading ? (
-        <p>Your content is loading, please wait...</p>
-      ) : (
-        <button onClick={() => clickHandler()}>Close</button>
-      )}
+
+      <Iframe
+        src="https://www.youtube.com/embed/Ctvzf_p0qUA?start=5&autoplay=1"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></Iframe>
+      <main>
+        {isLoading ? (
+          <p>Your content is loading, please wait...</p>
+        ) : (
+          <section>
+            <p>Your content has loaded, you can close this now</p>
+            <Button onClick={() => clickHandler()}>Close</Button>
+          </section>
+        )}
+      </main>
     </Div>
   );
 };
--- src/components/Loading/style.js
+++ src/components/Loading/style.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const Div = styled.div`
   background-color: #5555ff99;
   width: 80%;
+  max-width: 1000px;
   height: 80vh;
   position: absolute;
   top: 50%;
@@ -13,4 +14,21 @@ export const Div = styled.div`
   z-index: 1000;
 `;
 
-export const Iframe = styled.iframe``;
+export const Iframe = styled.iframe`
+  width: 100%;
+  aspect-ratio: 16/9;
+`;
+
+export const Button = styled.button`
+  background-color: #55a5ff;
+  color: white;
+  border: none;
+  border-radius: 5px;
+  font-size: 2rem;
+  padding: 0.5rem 1rem;
+  cursor: pointer;
+  transition: 200ms ease;
+  &:hover {
+    background-color: #55a599;
+  }
+`;
